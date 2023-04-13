9fa477e36109347478e284d807ed869d43f853ce
nguyenn02 <dinhnguyennguyen10082002@gmail.com>
2023-04-14 01:02:24 +0700

    xoa thanh vien

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
 from typing import Union
 import pymongo
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -32,5 +33,58 @@ def add_new_user(name:str, birth:str, gender:bool, weight:float, height:float, l
         a=db.members.insert_one({'name': name, 'birth': birth, 'gender': gender, 'weight': weight, 'height': height, 'level':level})
         print(a.inserted_id)
     except:
-        print("loi")
+        raise HTTPException(status_code=400, detail="sai thong tin")
     return {"OK": "thanh cong"}
+@app.get("/get_one")
+def get_one(name:str):
+    try:
+        client=pymongo.MongoClient("mongodb+srv://nguyen:Nguyencony02@database.rcdfbvy.mongodb.net/?retryWrites=true&w=majority")
+        db = client["basketball"]
+        user = db.members.find_one({'name':name})
+        if user is None:
+            raise HTTPException(status_code=400, detail="khong tim thay ten")
+    except:
+        raise HTTPException(status_code=400, detail="khong tim thay ten")
+    return {
+        "id":str(user['_id']),
+        "name":user['name'],
+        "birth":user['birth'],
+        "gender":user['gender'],
+        "weight":user['weight'],
+        "height":user['height'],
+        "level":user['level'],
+        }
+
+@app.get("/get_many")
+def get_many(name:str, gender:bool, birth:str):
+    try:
+        client=pymongo.MongoClient("mongodb+srv://nguyen:Nguyencony02@database.rcdfbvy.mongodb.net/?retryWrites=true&w=majority")
+        db = client["basketball"]
+        users = db.members.find({'name':name, 'gender':gender, 'birth':birth})
+        if users is None:
+          raise HTTPException(status_code=400, detail="khong tim thay thong tin")
+        print(users)
+        res=json.dumps(list(users),default=str)
+        return res 
+    except:
+        raise HTTPException(status_code=400, detail="sai thong tin")
+
+@app.delete("/delete")
+def delete(name:str):
+    try: 
+        client=pymongo.MongoClient("mongodb+srv://nguyen:Nguyencony02@database.rcdfbvy.mongodb.net/?retryWrites=true&w=majority")
+        db = client["basketball"]
+        user = db.members.find_one({'name':name})
+        if user is None:
+            raise HTTPException(status_code=400, detail="khong tim thay ten")
+        db.members.delete_one({"_id":user['_id']})
+    except:
+        raise HTTPException(status_code=400, detail="sai thong tin")
+    return {"Da xoa thong tin thanh vien"}
+    
+
+
+    
+
+
+ 
