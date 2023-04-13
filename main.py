7f593d213e7b9c85577ee2b1573971bc71bc88f3
nguyenn02 <dinhnguyennguyen10082002@gmail.com>
2023-04-13 23:17:13 +0700

    add user vào database

--- main.py
+++ main.py
@@ -14,13 +14,23 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.on_event("startup")
-def viet_nam():
+# @app.on_event("startup")
+# def tao_database():
+#     try:
+#         client=pymongo.MongoClient("mongodb+srv://nguyen:Nguyencony02@database.rcdfbvy.mongodb.net/?retryWrites=true&w=majority")#kết nối với pymongo
+#         db = client["basketball"]#tạo database
+#         # user = db.create_collection("members")#tạo ra 1 collection
+#         db.members.insert_one({'ten': "Nguyen", 'tuoi': 20})#tạo ra 1 document
+#         print("da ket noi")
+#     except:
+#         print("khong the ket noi")
+@app.post("/add")
+def add_new_user(name:str, birth:str, gender:bool, weight:float, height:float, level:str):
     try:
         client=pymongo.MongoClient("mongodb+srv://nguyen:Nguyencony02@database.rcdfbvy.mongodb.net/?retryWrites=true&w=majority")
         db = client["basketball"]
-        # user = db.create_collection("members")
-        db.members.insert_one({'ten': "Nguyen", 'tuoi': 20})
-        print("da ket noi")
+        a=db.members.insert_one({'name': name, 'birth': birth, 'gender': gender, 'weight': weight, 'height': height, 'level':level})
+        print(a.inserted_id)
     except:
-        print("khong the ket noi")
+        print("loi")
+    return {"OK": "thanh cong"}
