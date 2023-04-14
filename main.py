2327c61cc947b9e3cfe58f0b04a00f076e320b3d
nguyenn02 <dinhnguyennguyen10082002@gmail.com>
2023-04-14 22:07:01 +0700

    crud

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 from typing import Union
 import pymongo
 from fastapi import FastAPI, HTTPException
+from bson import ObjectId, json_util
 
 app = FastAPI()
 
@@ -11,9 +12,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
 
 # @app.on_event("startup")
 # def tao_database():
@@ -25,16 +23,17 @@ def read_item(item_id: int, q: Union[str, None] = None):
 #         print("da ket noi")
 #     except:
 #         print("khong the ket noi")
+
 @app.post("/add")
 def add_new_user(name:str, birth:str, gender:bool, weight:float, height:float, level:str):
     try:
         client=pymongo.MongoClient("mongodb+srv://nguyen:Nguyencony02@database.rcdfbvy.mongodb.net/?retryWrites=true&w=majority")
         db = client["basketball"]
-        a=db.members.insert_one({'name': name, 'birth': birth, 'gender': gender, 'weight': weight, 'height': height, 'level':level})
-        print(a.inserted_id)
+        db.members.insert_one({'name': name, 'birth': birth, 'gender': gender, 'weight': weight, 'height': height, 'level':level})
     except:
-        raise HTTPException(status_code=400, detail="sai thong tin")
+        raise HTTPException(status_code=400, detail="error")
     return {"OK": "thanh cong"}
+
 @app.get("/get_one")
 def get_one(name:str):
     try:
@@ -55,19 +54,19 @@ def get_one(name:str):
         "level":user['level'],
         }
 
-@app.get("/get_many")
-def get_many(name:str, gender:bool, birth:str):
-    try:
-        client=pymongo.MongoClient("mongodb+srv://nguyen:Nguyencony02@database.rcdfbvy.mongodb.net/?retryWrites=true&w=majority")
-        db = client["basketball"]
-        users = db.members.find({'name':name, 'gender':gender, 'birth':birth})
-        if users is None:
-          raise HTTPException(status_code=400, detail="khong tim thay thong tin")
-        print(users)
-        res=json.dumps(list(users),default=str)
-        return res 
-    except:
-        raise HTTPException(status_code=400, detail="sai thong tin")
+# @app.get("/get_many")
+# def get_many(name:str, gender:bool, birth:str):
+#     try:
+#         client=pymongo.MongoClient("mongodb+srv://nguyen:Nguyencony02@database.rcdfbvy.mongodb.net/?retryWrites=true&w=majority")
+#         db = client["basketball"]
+#         users = db.members.find({'name':name, 'gender':gender, 'birth':birth})
+#         if users is None:
+#           raise HTTPException(status_code=400, detail="khong tim thay thong tin")
+#         print(users)
+#         res=json.dumps(list(users),default=str)
+#         return res 
+#     except:
+#         raise HTTPException(status_code=400, detail="sai thong tin")
 
 @app.delete("/delete")
 def delete(name:str):
@@ -81,10 +80,29 @@ def delete(name:str):
     except:
         raise HTTPException(status_code=400, detail="sai thong tin")
     return {"Da xoa thong tin thanh vien"}
+     
+@app.get("/get_all")
+def get_all():
+    try:
+        client=pymongo.MongoClient("mongodb+srv://nguyen:Nguyencony02@database.rcdfbvy.mongodb.net/?retryWrites=true&w=majority")
+        db = client["basketball"]
+        users=db.members.find({})
+    except:
+        raise HTTPException(status_code=400, detail="error")
+    res=json.dumps(list(users),default=json_util.default)
+    return res 
+    
+@app.put("/update")
+def update(_id:str, name:str, birth:str, gender:bool, weight:float, height:float, level:str):
+      try:
+        client=pymongo.MongoClient("mongodb+srv://nguyen:Nguyencony02@database.rcdfbvy.mongodb.net/?retryWrites=true&w=majority")
+        db = client["basketball"]
+        db.members.update_one({'_id':ObjectId(_id)}, {'$set':{'name': name, 'birth': birth, 'gender': gender, 'weight': weight, 'height': height, 'level':level}})       
+      except:
+          raise HTTPException(status_code=400, detail="error")
+      return {"update thanh cong"}
     
 
+        
 
-    
 
-
- 
